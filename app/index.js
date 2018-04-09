a9cf1bb3fab16a0d5d51c61de1d438ebcad0d852
Nadiajda Cavalcanti <nadicavalcanti@gmail.com>
2018-04-08 23:11:03 -0300

    Screens Option e Theme

--- app/index.js
+++ app/index.js
@@ -13,7 +13,7 @@ EStyleSheet.build({
 });
 
 export default () => <Home />;
-*/
+
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
@@ -29,3 +29,23 @@ EStyleSheet.build({
 });
 
 export default () => <CurrencyList />;
+*/
+
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import Themes from './screens/Themes';
+
+EStyleSheet.build({
+  $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
+  $white: '#FFFFFF',
+  $lightGray: '#F0F0F0',
+  $border: '#979797',
+  $inputText: '#797979',
+  $darkText: '#343434',
+});
+
+export default () => <Themes />;
